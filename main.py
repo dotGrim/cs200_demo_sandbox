81036f79eeddef747da4fb573bc6e4d48b794eff
Grim <dotgrim12@gmail.com>
2026-03-25 10:57:51 -0400

    Refactored

--- main.py
+++ main.py
@@ -14,15 +14,18 @@ def func2(data):
     choice = ''
     while True:
         choice = input('Select an option. 1-sum  2-min  3-max  4-quit')
-        if choice == '1':
-            print('The sum is',sum(data))
-        elif choice == '2':
-            print('Not supported yet')
-        elif choice == '3':
-            print('Not supported yet')
-        elif choice == '4':
-            print('Goodbye')
-            return
+        choiceOption(data, choice)
+
+def choiceOption(data, choice):
+    if choice == '1':
+        print(f"The sum is {sum(data)}")
+    elif choice == '2':
+        print(f"The min is {min(data)}")
+    elif choice == '3':
+        print(f"Not supported yet")
+    elif choice == '4':
+        print('Goodbye')
+        exit(0)
 
 def main():
     print("Hello, World!")
